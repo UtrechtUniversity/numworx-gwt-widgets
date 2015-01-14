fe5f8161e36880bdb0dae593183e9680923f2e29
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-01-14 15:51:16 +0000

    constanten voor de verschillende edit-acties
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/StatistiekGWT/trunk@8819 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiekgwt/client/event/TableChangeEvent.java
+++ src/fi/statistiekgwt/client/event/TableChangeEvent.java
@@ -5,6 +5,21 @@ import com.google.gwt.event.shared.GwtEvent;
 public class TableChangeEvent extends GwtEvent<TableChangeEventHandler>
 {
 	public static Type<TableChangeEventHandler> TYPE = new Type<TableChangeEventHandler>();
+	
+	public static String REMOVE_ROWS = "removeRows";
+	public static String VIEWS_EDITABLE = "viewsEditable";
+	public static String DATA_EDITABLE = "dataEditable";
+	public static String VIEWS_ADDABLE = "viewsAddable";
+	public static String SET_COLUMN_NAME = "setColumnName";
+	public static String SET_VALUE_AT = "setValueAt";
+	public static String ADD_ROW = "addRow";
+	public static String ADD_COLUMN = "addColumn";
+	public static String EDIT_COLUMN = "editColumn";
+	public static String REMOVE_COLUMN = "removeColumn";
+	public static String REMOVE_ROW = "removeRow";
+	public static String SORT_COLUMN = "sortColumn";
+	public static String UPDATE_NUMERICAL_COLUMN_TYPES = "updateNumericalColumnTypes";
+	public static String IMPORT_CSV = "importCSV";
 
 	private final String info;
 
