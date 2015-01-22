0c27a76fe5bbf72c785cda1931ad5152363f7acd
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-01-22 15:26:47 +0000

    initial commit
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/StatistiekGWT/trunk@8849 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/statistiekgwt/client/event/ViewSelectionChangeEventHandler.java
@@ -0,0 +1,8 @@
+package fi.statistiekgwt.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface ViewSelectionChangeEventHandler extends EventHandler
+{
+	void onViewSelectionChange(ViewSelectionChangeEvent event);
+}
