33f5ee74b5b31fc3476d233ec17179839d7da833
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-04-10 14:39:49 +0000

    commentaar toegevoegd
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/StatistiekGWT/trunk@9467 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiekgwt/client/StatistiekUtils.java
+++ src/fi/statistiekgwt/client/StatistiekUtils.java
@@ -4,6 +4,12 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.SimpleEventBus;
 
+/**
+ * Class that provides utility classes.
+ * 
+ * @author borku102
+ * 
+ */
 public class StatistiekUtils
 {
 	public static EventBus EVENT_BUS = GWT.create(SimpleEventBus.class);
