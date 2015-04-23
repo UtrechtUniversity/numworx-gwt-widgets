19510c734825a6fee01d23c9b4010501edeaafc1
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-04-23 15:11:23 +0000

    layout met flowpanels; accolades toegevoegd; commentaar opgeruimd
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/StatistiekGWT/trunk@9607 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/statistiekgwt/client/ChangeViewNameDialog.java
+++ src/fi/statistiekgwt/client/ChangeViewNameDialog.java
@@ -2,6 +2,7 @@ package fi.statistiekgwt.client;
 
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.TextAlign;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -9,6 +10,8 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.touch.client.Point;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -33,11 +36,11 @@ public class ChangeViewNameDialog extends DialogBox // implements ActionListener
 	
 	private Label nameLabel;
 	private TextBox nameField;
-	private LayoutPanel namePanel;
+	private FlowPanel namePanel;
 	private Button okButton;
 	private Button cancelButton;
-	private LayoutPanel okCancelPanel;
-	private LayoutPanel alles;
+	private HorizontalPanel okCancelPanel;
+	private FlowPanel alles;
 	
 	private StatModel model;
 	private String viewName;
@@ -64,7 +67,6 @@ public class ChangeViewNameDialog extends DialogBox // implements ActionListener
 		StatInteractiePanelView view)
 	{
 		super(true, true);
-		super.setPixelSize(ChangeViewNameDialog.DEFAULT_WIDTH, ChangeViewNameDialog.DEFAULT_HEIGHT);
 		super.setText(StatistiekGWT.rb.getString("changeviewnameDialog"));
 		this.viewName = viewName;
 		this.model = model;
@@ -91,84 +93,43 @@ public class ChangeViewNameDialog extends DialogBox // implements ActionListener
 	private void setUp()
 	{
 		this.nameLabel = new Label(StatistiekGWT.rb.getString("enternameLabel"));
+		this.nameLabel.addStyleName(statistiekCss.spaceBottomLabel());
 		this.nameField = new TextBox();
 		this.nameField.setText(this.viewName);
 		this.nameField.addStyleName(statistiekCss.textbox());
+		this.nameField.addStyleName(statistiekCss.spaceBottomLabel());
 
-		this.namePanel = new LayoutPanel();
+		this.namePanel = new FlowPanel();
 		this.namePanel.add(this.nameLabel);
 		this.namePanel.add(this.nameField);
-		// set position
-		this.namePanel.setWidgetLeftWidth(this.nameLabel, 0, Style.Unit.PCT, 100, Style.Unit.PCT);
-		this.namePanel.setWidgetTopHeight(this.nameLabel, 0, Style.Unit.PCT, 50, Style.Unit.PCT);
-		this.namePanel.setWidgetLeftWidth(this.nameField, 0, Style.Unit.PCT, 100, Style.Unit.PCT);
-		this.namePanel.setWidgetTopHeight(this.nameField, 50, Style.Unit.PCT, 50, Style.Unit.PCT);
 		
 		this.okButton = new Button(StatistiekGWT.rb.getString("OKButtonText"));
+		this.okButton.addStyleName(statistiekCss.margin());
 		this.cancelButton = new Button(StatistiekGWT.rb.getString("cancelButtonText"));
-		this.okCancelPanel = new LayoutPanel();
+		this.cancelButton.addStyleName(statistiekCss.margin());
+		
+		this.okCancelPanel = new HorizontalPanel();//LayoutPanel();
+		this.okCancelPanel.addStyleName(statistiekCss.horizontalPanelWithoutBorder());
+		this.okCancelPanel.getElement().getStyle().setTextAlign(TextAlign.CENTER);
 		this.okCancelPanel.add(this.okButton);
 		this.okCancelPanel.add(this.cancelButton);
-		// set position
-		this.okCancelPanel.setWidgetLeftWidth(this.okButton, 25, Style.Unit.PCT, 25, Style.Unit.PCT);
-		this.okCancelPanel.setWidgetTopHeight(this.okButton, 0, Style.Unit.PX, 30, Style.Unit.PX);
-		this.okCancelPanel.setWidgetLeftWidth(this.cancelButton, 50, Style.Unit.PCT, 25, Style.Unit.PCT);
-		this.okCancelPanel.setWidgetTopHeight(this.cancelButton, 0, Style.Unit.PX, 30, Style.Unit.PX);
 		
-		this.alles = new LayoutPanel();
-		this.alles.setPixelSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-
+		this.alles = new FlowPanel();//LayoutPanel();
+		
 		this.alles.add(this.namePanel);
 		this.alles.add(this.okCancelPanel);
-		this.alles.setPixelSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
-		// set position
-		this.alles.setWidgetLeftWidth(this.namePanel, 0, Unit.PCT, 100, Unit.PCT);  // Top panel
-		this.alles.setWidgetTopHeight(this.namePanel, 0, Style.Unit.PX, 70, Style.Unit.PX);
-		this.alles.setWidgetLeftWidth(this.okCancelPanel, 0, Unit.PCT, 100, Unit.PCT); // Bottom panel
-		this.alles.setWidgetTopHeight(this.okCancelPanel, 70, Style.Unit.PX, 30, Style.Unit.PX);
 		this.add(this.alles);
 	}
 
-//	public void actionPerformed(ActionEvent arg0)
-//	{
-//		if (arg0.getSource() == this.textField)
-//		{
-//			boolean b = true;
-//			for (int i = 0; b && i < this.model.getViews().size(); i++)
-//			{
-//				b = i == this.viewIndex
-//					|| !this.model.getViews().get(i).getViewName()
-//						.equals(this.textField.getText());
-//			}
-//			if (b)
-//			{
-//				this.model.getViews().get(this.viewIndex)
-//					.setViewName(this.textField.getText());
-//
-//				this.view.update();
-//				// set the currently selected view
-//				// syl: tabPane is not showing correctly,
-//				// while the field selectedView is properly updated (e.g.,
-//				// change page to check this)
-//				this.view.processSelectedView(this.viewIndex);
-//
-//				super.setVisible(false);
-//			}
-//			else
-//			{
-//				this.nameLabel.setText(StatistiekGWT.rb
-//					.getString("namealreadyinuseLabel"));
-//			}
-//		}
-//	}
-	
 	private boolean hasChanged()
 	{
 		boolean hasChanged = false;
 		
 		if (!this.nameField.getText().equals(viewName))
+		{
 			hasChanged = true;
+		}
 		
 		return hasChanged;
 	}
