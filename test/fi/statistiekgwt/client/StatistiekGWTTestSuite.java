4a2d16650c097b83b53d15d1c2b42b0ff9ac31be
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-08-27 09:32:35 +0000

    test suite voor StatistiekGWT tests
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/StatistiekGWT/trunk@10334 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ test/fi/statistiekgwt/client/StatistiekGWTTestSuite.java
@@ -0,0 +1,22 @@
+package fi.statistiekgwt.client;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import fi.statistiekgwt.client.descriptives.DescriptivesModelTest;
+import fi.statistiekgwt.client.descriptives.DescriptivesViewTest;
+
+public class StatistiekGWTTestSuite extends GWTTestSuite
+{
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite("Test for a StatistiekGWT Application");
+		suite.addTestSuite(StatistiekGWTTest.class);
+		suite.addTestSuite(StatTableModelTest.class);
+		suite.addTestSuite(DescriptivesModelTest.class);
+		suite.addTestSuite(DescriptivesViewTest.class);
+		return suite;
+	}
+}
