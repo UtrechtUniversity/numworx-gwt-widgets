42e0a22408df57788f625f479d76e8fa58e47b3f
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2015-09-11 11:08:34 +0000

    Stub.publish()
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenGWT/trunk@10401 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/algebrapijlengwt/client/AlgebraPijlenGWT.java
+++ src/fi/algebrapijlengwt/client/AlgebraPijlenGWT.java
@@ -153,8 +153,8 @@ public class AlgebraPijlenGWT implements EntryPoint, InteractionStub //Interacti
 	
 		standAlone = true;
 		
-		//Stub.publish(this);
-		init(breedte, hoogte, new HashMap<String, Object>(), new HashMap<String, Number>());
+		Stub.publish(this);
+		//init(breedte, hoogte, new HashMap<String, Object>(), new HashMap<String, Number>());
 			
 	}
 
