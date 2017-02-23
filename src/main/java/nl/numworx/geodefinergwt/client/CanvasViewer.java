06e8c6106e935e394ce9b021e93ce480ef43ca38
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-02-23 10:00:24 +0000

    canvas viewer
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/GeoDefinerGWT/trunk@151 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefinergwt/client/CanvasViewer.java
+++ src/main/java/nl/numworx/geodefinergwt/client/CanvasViewer.java
@@ -8,11 +8,19 @@ import org.vectomatic.dom.svg.utils.SVGConstants;
 import com.google.gwt.animation.client.AnimationScheduler;
 import com.google.gwt.animation.client.AnimationScheduler.AnimationCallback;
 import com.google.gwt.animation.client.AnimationScheduler.AnimationHandle;
+import com.google.gwt.canvas.dom.client.CssColor;
 
 import nl.numworx.geodefiner.common.Snapper;
+import nl.numworx.geodefiner.common.Tips;
+import nl.numworx.geodefinergwt.client.ui.ColorStyle;
+import nl.numworx.geodefinergwt.client.ui.FillStyle;
+import nl.numworx.geodefinergwt.client.ui.StrokeStyle;
 import fi.euclides.gwt.canvas.SpeelVeld;
+import fi.euclides.model.Destroyable;
 import fi.euclides.model.Punt;
+import fi.euclides.model.Segment;
 import fi.euclides.model.Triangle;
+import fi.euclides.util.Adapter;
 import fi.euclides.util.DefaultAdapter;
 import gwt.awt.Shape;
 import gwt.awt.geom.Path2D;
@@ -50,6 +58,7 @@ public class CanvasViewer extends SpeelVeld implements SnapperImpl.PH {
 	}
 
 	SnapperImpl snapper = new SnapperImpl();
+	private StrokeStyle stroke;
 	@Override
 	public void processMouseUp(int x0, int y0) {
 		snapper.pmUp(x0, y0, this);
@@ -83,7 +92,6 @@ public class CanvasViewer extends SpeelVeld implements SnapperImpl.PH {
 		new AxesDrawer(this).drawAxes();
 	}
 
-	String fill = "white";
 	
 	@Override
 	public void visitTriangle(Triangle t) {
@@ -107,8 +115,90 @@ public class CanvasViewer extends SpeelVeld implements SnapperImpl.PH {
 		path.closePath();
 		context.setFillStyle(fill);
 		context.fill();
+		if(stroke != null) stroke.toStyle(context);
 		context.stroke();
 		DefaultAdapter.getDefault(t).put(Shape.class, path);
 	}
 
+	public void selectColor(Destroyable object) {
+		if(tracking || trail)
+			return;
+		Adapter a = object.getAdapter();
+		stroke = a.adapt(StrokeStyle.class);
+		context.setLineWidth(1);
+		//context.setLineDash(null)
+		FillStyle f = a.adapt(FillStyle.class);
+		if(f == null) fill = "none";
+		else fill = f.getColor();
+		
+		ColorStyle c = a.adapt(ColorStyle.class);
+		if (c != null) {
+			// if selected?
+			setCssColor(CssColor.make(c.getColor()));
+			return;
+		}
+		super.selectColor(object);
+	}
+
+	@Override
+	public void drawLine(double x1, double y1, double x2, double y2) {
+		if(stroke != null) stroke.toStyle(context);
+		super.drawLine(x1, y1, x2, y2);
+	}
+
+	@Override
+	protected void drawCircle(double i, double j, double k) {
+		if(stroke != null) stroke.toStyle(context);
+		super.drawCircle(i, j, k);
+	}
+
+	@Override
+	protected void drawArc(double x, double y, double k, double start,
+			double length) {
+		if(stroke != null) stroke.toStyle(context);
+		super.drawArc(x, y, k, start, length);
+	}
+
+	private Segment drawTips(Segment s) {
+		Tips tip = s.adapt(Tips.class);
+		if(tip == null) return s;
+		selectColor(s);
+		double dx = s.getDX();
+		double dy = s.getDY();
+		double len = Math.hypot(dx, dy);
+		Float width = s.adapt(Float.class);
+		double tiplen = 5;
+		if(width != null) tiplen *= width.doubleValue();
+		if(len < tiplen*3) tiplen = len/3;
+		dx *= tiplen/len; 
+		dy *= tiplen/len; 
+		switch(tip) {
+		case ATEND: tip(s.getP2(), -dx, -dy); break;
+		case ATSTARTEND: tip(s.getP2(),-(dx), -(dy));
+		case ATSTART: tip(s.getP1(), dx, dy);
+		case NOTIP: 
+		}
+		return s;
+	}
+
+	private void tip(Punt p1, double dx, double dy) {
+		double x = (float)p1.getXd();
+		double y = (float)p1.getYd();
+		context.beginPath();
+		context.moveTo(x, y);
+		context.lineTo(x + dx + dy/2, y + dy -dx/2);
+		context.lineTo(x + dx - dy/2, y + dy +dx/2);
+		context.closePath();
+		context.fill();
+	}
+
+	@Override
+	public void visitSegment(Segment s) {
+		s = drawTips(s);
+		super.visitSegment(s);
+		
+	}
+
+	
+	
 }
--- src/main/java/nl/numworx/geodefinergwt/client/ui/StrokeStyle.java
+++ src/main/java/nl/numworx/geodefinergwt/client/ui/StrokeStyle.java
@@ -3,21 +3,36 @@ package nl.numworx.geodefinergwt.client.ui;
 import org.vectomatic.dom.svg.OMSVGStyle;
 import org.vectomatic.dom.svg.utils.SVGConstants;
 
+import com.google.gwt.canvas.dom.client.Context2d;
+
 public class StrokeStyle implements SVGConstants {
 
 	final String width;
 	final String dasharray;
+	final double lineWidth, dash[];
 
-	StrokeStyle(String w, String d) {
+	StrokeStyle(String w, String d, double lw, double dash[]) {
 		width = w;
 		dasharray = d;
+		lineWidth = lw;
+		this.dash = dash;
 	}
 
+	StrokeStyle(String w, String d) {
+		this(w,d,Double.parseDouble(w), null);
+	}
+	
+	
 	public void toStyle(OMSVGStyle style) {
 		if (width != null)
 			style.setSVGProperty(CSS_STROKE_WIDTH_PROPERTY, width);
 		if (dasharray != null)
 			style.setSVGProperty(CSS_STROKE_DASHARRAY_PROPERTY, dasharray);
+	}
+
+	public void toStyle(Context2d context) {
+		context.setLineWidth(lineWidth);
+		//context.setLineDash(dash);
 	}
 	
 }
