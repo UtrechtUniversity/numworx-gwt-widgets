817eeaad3326c7655e6b432cdd5b842503bf422e
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2017-03-08 10:39:47 +0000

    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/GeoDefinerGWT/trunk@176 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefinergwt/client/CanvasViewer.java
+++ src/main/java/nl/numworx/geodefinergwt/client/CanvasViewer.java
@@ -303,7 +303,7 @@ public class CanvasViewer extends SpeelVeld implements SnapperImpl.PH {
 		DEFAULT_STROKE.toStyle(context);
 		context.strokeRect(x, y, 10, 10);
 		String string = getMapper().toString(label);
-		drawString(string, x+12, y+10);
+		drawString(string, x+12, y+10, null, null, null);
 		RectShape r = new RectShape(x, y, 12 + context.measureText(string).getWidth(), 10);
 		DefaultAdapter.getDefault(label).put(Shape.class, r);
 	}
