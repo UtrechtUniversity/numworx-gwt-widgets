dbe7dd37c462ada07d5ec37ba7552336d3dbf4de
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-06-19 11:56:11 +0000

    fix casNodig
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenGWT/trunk@16632 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/algebrapijlengwt/client/UitvoerSchuifComponent.java
+++ src/fi/algebrapijlengwt/client/UitvoerSchuifComponent.java
@@ -921,8 +921,13 @@ public class UitvoerSchuifComponent extends AlgebraSchuifComponent //implements 
 					isGeldigeInvoer = false;
 				}
 				else
-				{
-					exp = fi.wiskopdr.expressies.Expressie.evalWithCAS(exp); // deze kan een restartexception geven
+				{ 
+					boolean casNodig = false;
+					if (exp!=null) 
+						casNodig = exp.toString().indexOf("$i")>-1 || exp.toString().indexOf("$d")>-1 || exp.toString().indexOf("$T")>-1  || exp.toString().indexOf("$S")>-1  || exp.toString().indexOf("$P")>-1;
+					//logger.fine(antwoord + " needs " + casNodig);
+					if (casNodig)
+						exp = fi.wiskopdr.expressies.Expressie.evalWithCAS(exp); // deze kan een restartexception geven
 					Double d = null;
 					if (exp != null && exp.isWaarde())
 					{
@@ -968,7 +973,7 @@ public class UitvoerSchuifComponent extends AlgebraSchuifComponent //implements 
 					tf.setText("");
 					tfString = "";
 				}
-			}			
+			}
 		}
 		
 		if (isGeldigeInvoer)
