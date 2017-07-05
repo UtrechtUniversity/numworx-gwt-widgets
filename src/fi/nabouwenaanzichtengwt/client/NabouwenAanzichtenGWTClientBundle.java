60cd112b72248c38e71f387331eed08a00c3d4e0
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-07-05 09:40:19 +0000

    fix DWOWIDGET-99 nakijkvinkje met css-stijl
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/NabouwenAanzichtenGWT@16841 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/nabouwenaanzichtengwt/client/NabouwenAanzichtenGWTClientBundle.java
+++ src/fi/nabouwenaanzichtengwt/client/NabouwenAanzichtenGWTClientBundle.java
@@ -7,25 +7,6 @@ import com.google.gwt.resources.client.ClientBundle.Source;
 
 public interface NabouwenAanzichtenGWTClientBundle extends ClientBundle 
 {
-
-   @Source("fi/nabouwenaanzichtengwt/client/resources/footerbgimage.png")
-   public ImageResource footerbgimage();
-   
-   @Source("fi/nabouwenaanzichtengwt/client/resources/vinkjegrijs.png")
-   public ImageResource vinkjegrijs();
-   
-   @Source("fi/nabouwenaanzichtengwt/client/resources/vinkjegeel.png")
-   public ImageResource vinkjegeel();
-   
-   @Source("fi/nabouwenaanzichtengwt/client/resources/vinkjerood.png")
-   public ImageResource vinkjerood();
-   
-   @Source("fi/nabouwenaanzichtengwt/client/resources/vinkje.png")
-   public ImageResource vinkje();
-
-  
-   
    @Source("fi/nabouwenaanzichtengwt/client/resources/NabouwenAanzichtenGWT.css")
    public  NabouwenAanzichtenGWTCssResource getNabouwenAanzichtenGWTCSS();
-
 }
