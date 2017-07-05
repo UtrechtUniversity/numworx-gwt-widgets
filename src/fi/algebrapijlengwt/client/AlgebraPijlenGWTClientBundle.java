f9d1718625daef2485957979c576e896ecfe2a09
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-07-05 11:04:31 +0000

    code opgeschoond
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/AlgebraPijlenGWT/trunk@16848 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/algebrapijlengwt/client/AlgebraPijlenGWTClientBundle.java
+++ src/fi/algebrapijlengwt/client/AlgebraPijlenGWTClientBundle.java
@@ -6,20 +6,6 @@ import com.google.gwt.resources.client.ClientBundle.Source;
 
 public interface AlgebraPijlenGWTClientBundle extends ClientBundle
 {
-     //@Source("fi/algebrapijlengwt/client/images/foutkruis.gif")
-	 //public ImageResource foutKruisResource();
-
-	 //@Source("fi/algebrapijlengwt/client/images/goedkrul_en.gif")
-	 //public ImageResource goedKrulEnResource();
-
-	 //@Source("fi/algebrapijlengwt/client/images/goedkrul.gif")
-	 //public ImageResource goedKrulResource();
-	 
-	 //@Source("fi/algebrapijlengwt/client/images/goedkrulhalf.gif")
-	 //public ImageResource goedKruHalflResource();
- 
-	
 	 @Source("fi/algebrapijlengwt/client/css/AlgebraPijlenGWT.css")
 	 public AlgebraPijlenGWTCssResource getAlgebraPijlenGWTCSS();
-
 }
