23bba00c49b00061dadca5adca61b44f331691a7
kuipe036 <kuipe036@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2017-08-22 13:41:37 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/GraphToolGWT/branches/jira117@17235 58dd2db4-0f11-0410-9df4-da15f00f50bd

---
+++ src/fi/graphtoolgwt/client/text/Text_en.java
@@ -0,0 +1,63 @@
+package fi.graphtoolgwt.client.text;
+
+import java.util.Vector;
+
+public class Text_en {
+
+	
+	private Vector<String> keys = new Vector<String>();
+	private Vector<String> values = new Vector<String>();
+
+	public Text_en()
+	{
+		Object[][] items = this.getContents();
+		for (int i = 0; i < items.length; i++)
+		{
+			keys.add(items[i][0].toString());
+			values.add(items[i][1].toString());
+		}
+	}
+
+	public Object[][] getContents()
+	{
+		return contents;
+	}
+
+	public String getString(String key)
+	{
+		int keyint = keys.indexOf(key);
+		return values.get(keyint);
+	}
+
+	static final Object[][] contents =
+	{
+	{ 	"ofLabel", "of" },
+	{ 	"ofLabel", "or" },
+	{	"enOfButton_En", "en"},
+	{	"enOfButton_En", "and"},
+	{	"enOfButton_Of", "of"},
+	{	"enOfButton_Of", "or"},
+	
+	{	"kijkNaButton", "Klaar"},
+	{	"kijkNaButton", "Ready"},
+	
+	{	"feedbackTekstPuntenDeels", "Nog niet alle punten zijn (goed) getekend."},
+	{	"feedbackTekstPuntenDeels", "Not all points are drawn (correctly) yet."},
+    {	"feedbackTekstGrafiekenDeels", "Nog niet alle grafieken zijn goed getekend."},
+    {	"feedbackTekstGrafiekenDeels", "Not all graphs are drawn (correctly) yet."},
+    {	"feedbackTekstLabelsAssen", "Zet de juiste letters bij de assen"},
+    {	"feedbackTekstLabelsAssen", "Place the correct letters to the axes"},
+    {	"feedbackTekstTekenGrafiek", "Let op: teken ook de grafiek."},
+    {	"feedbackTekstTekenGrafiek", "Pay attention: also draw the graph."},
+    {	"feedbackTekstTeWeinigPunten", "Let op: je hebt nog niet voldoende punten getekend."},
+    {	"feedbackTekstTeWeinigPunten", "Pay attention: an insufficient amount of points are drawn."},
+    	
+	{	"fc_huidigDomein", "Domein is nu: "},
+	{	"fc_huidigDomein", "The domain is now: "},
+    {	"fc_domeinBijFunctie", "Domein bij functie"},
+    {	"fc_domeinBijFunctie", "Function domain"},
+    {	"fc_nieuwDomein", "Nieuw domein: "},
+    {	"fc_nieuwDomein", "New domain: "},
+	
+	};
+}
