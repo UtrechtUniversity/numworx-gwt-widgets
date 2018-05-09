1a4f6a9e380526d371e5848064089b64a4f426b7
borku102 <borku102@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2018-05-09 11:45:21 +0000

    DWOWIDGET-127 DWO-toetsenbord gebruiken; code/commentaar opgeschoond
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/HeksGWT/branches/sylvia@20126 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/heksgwt/client/Pagina24Panel.java
+++ src/fi/heksgwt/client/Pagina24Panel.java
@@ -1,31 +1,23 @@
 package fi.heksgwt.client;
 
-//import java.awt.*;
-//import java.applet.*;
-//import java.awt.event.*;
 import java.util.Map;
 import java.util.HashMap;
 import fi.heksgwt.client.scobjects.*;
-//import fi.beans.appletutil.*;
 import fi.heksgwt.client.vectortek.*;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.PushButton;
-import com.google.gwt.user.client.ui.PopupPanel;
-
-import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwt.canvas.dom.client.CssColor;
 
 
-public class Pagina24Panel extends ScContainer // implements MouseListener, MouseMotionListener, ActionListener 
+/**
+ * Pagina24 is het panel om te oefenen met blokjes erin en eruit.
+ * In instellingenpanel docentomgeving 'hulppagina', in HeksGWT paginanummer 4. 
+ * 
+ * @author borku102
+ *
+ */
+public class Pagina24Panel extends ScContainer 
 {
-	// private Heks eigenaar;
-	//private AppletUtil au;
-	//HeksInteractiePanel heip;
 	HeksGWT eigenaar;
-
-	//private AchtergrondContainer achtergrond;
 
 	Tekening blokjePlus, blokjeMin, blokjeSleep, blokjeSleepMin;
 	Tekening pot, potEruit, potErin, beginPot, eindPot, potinhoud, vloer, schrijfheks, werkheks;
@@ -33,13 +25,8 @@ public class Pagina24Panel extends ScContainer // implements MouseListener, Mous
 	GetalComponent tc, beginTemp, eindTemp;
 	Thermometer tm;
 	BlokjesContainer eruitContainer, erinContainer;
-	//ScLWButton opdrachtKnop, werkKnop;
 	ScLabel beginLabel, eindLabel, erinLabel, eruitLabel, titel, opdrachtTitel;
-	//ScTekstContainer uitleg, opdracht;
-	//ScPanel sleeppanel;
-	//ScTextArea textArea;
 	Polygon[] p;
-	// AudioClip plons, bubbel;
 	
 	Pagina24OefenPanel oefenTafereelPanel;
 	
@@ -48,34 +35,22 @@ public class Pagina24Panel extends ScContainer // implements MouseListener, Mous
 	boolean plusEruit, minEruit, pasEruit;
 	boolean[] kleurBlokjes = { true, false, true, true, true, true, false, false, false, false, true };
 
-	//ImageButton opnieuwKnop;
 	PushButton opnieuwKnop;
 
 	public Pagina24Panel(int x, int y, int b, int h, HeksGWT eigenaar) 
 	{
 		super(x, y, b, h);
-		//this.heip = heip;
 		this.eigenaar = eigenaar;
-		// setBackground(new Color(255,255,220));
-		//setBackground(heip.bgColor);
-		//zetVastePlaats(true);
 
-		//OefenTafereelPanel_WN oefenTafereelPanel = new OefenTafereelPanel_WN(0, 0, b, h, applet);
-//GWT		
 		oefenTafereelPanel = new Pagina24OefenPanel(0, 0, b, h, eigenaar, this);
 		oefenTafereelPanel.zetInstelbaar(true);
-		//oefenTafereelPanel.addActionListener(this);
-		//oefenTafereelPanel.zetEruitMogelijk(false);
 		add(oefenTafereelPanel);
-
-
 	}
 	
 	public void initHandlers()
 	{
 		oefenTafereelPanel.initHandlers();
 	}
-
 
 	public void setState(Map h) 
 	{
@@ -92,23 +67,7 @@ public class Pagina24Panel extends ScContainer // implements MouseListener, Mous
 
 	public void start() {}
 
-	public void stop() { // za.stop();
+	public void stop()
+	{
 	}
-
-//	public void mousePressed(MouseEvent e) {}
-
-//	public void mouseDragged(MouseEvent e) {}
-
-//	public void mouseReleased(MouseEvent e) {}
-
-//	public void mouseMoved(MouseEvent e) {}
-
-//	public void mouseExited(MouseEvent e) {;}
-
-//	public void mouseClicked(MouseEvent e) {}
-
-//	public void mouseEntered(MouseEvent e) {;}
-
-//	public void actionPerformed(ActionEvent e) {}
-
 }
