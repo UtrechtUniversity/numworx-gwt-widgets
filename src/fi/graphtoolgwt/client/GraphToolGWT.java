5e55e926ecba95372282ce1d8a27891ab1fd0b3d
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2020-01-16 12:06:40 +0000

    met pointer-events
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/GraphToolGWT/branches/pointer@24310 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/graphtoolgwt/client/GraphToolGWT.java
+++ src/fi/graphtoolgwt/client/GraphToolGWT.java
@@ -64,6 +64,7 @@ import com.google.web.bindery.event.shared.HandlerRegistrations;
 import com.googlecode.mgwt.dom.client.event.mouse.HandlerRegistrationCollection;
 import com.vaadin.pointerevents.client.PointerDownEvent;
 import com.vaadin.pointerevents.client.PointerDownHandler;
+import com.vaadin.pointerevents.client.PointerEvent;
 import com.vaadin.pointerevents.client.PointerMoveEvent;
 import com.vaadin.pointerevents.client.PointerMoveHandler;
 import com.vaadin.pointerevents.client.PointerUpEvent;
@@ -4514,28 +4515,67 @@ public class GraphToolGWT implements EntryPoint, InteractionStub, FacetAware, CB
 	
 	class PointerHandler implements PointerDownHandler, PointerMoveHandler, PointerUpHandler {
 
-		private boolean mouseDown;
+		private int state = READY;
+		private int id1, x1, y1, id2, x2, y2;
+
+		int pinchStartDistance = 0;
+		int pinchMoveDistance = 0;
+		int pinchMoveDirection = DIAGONAL;
 
 		@Override
 		public void onPointerUp(PointerUpEvent e) {
 			e.preventDefault();
 			GWT.log("pointer-up " + e.getX() + " " + e.getY()  + " " + e.getPointerId());
-			mouseDown = false;
-			mouseUpTouchEndAction(e.getSource(), e.getX(), e.getY());
+			if (state == ONE_FINGER && e.getPointerId() == id1) 
+			{
+				state = READY;
+				mouseUpTouchEndAction(e.getSource(), e.getX(), e.getY());
+			} else if (state == TWO_FINGERS) {
+				if (e.getPointerId() == id2) {
+					state = ONE_FINGER;
+				} else if (e.getPointerId() == id1) {
+					id1 = id2;
+					x1 = e.getX();
+					y1 = e.getY(); 
+					state = ONE_FINGER;
+				}
+				if (state == ONE_FINGER) {
+					mouseUpTouchEndAction(e.getSource(), e.getX(), e.getY());				
+				}
+				
+			}
 		}
 
 		@Override
 		public void onPointerMove(PointerMoveEvent e) {
+			if (others != null) {
+				others.removeHandler();
+				others = null;
+			}
 			e.preventDefault();
 			
-			if ( mouseDown && (tekenComponent.getCursorMode() != TekenComponentGWT.DELETE) ) {
+			if ( state == ONE_FINGER && (tekenComponent.getCursorMode() != TekenComponentGWT.DELETE) && e.getPointerId() == id1) {
 				// only move action needs to be taken during drag, draw or default mode
-				int eventX = e.getX();
-				int eventY = e.getY();
-				GWT.log("pointer-move " + eventX + " " + eventY  + " " + e.getPointerId());
-				
+				int eventX = x1 = e.getX();
+				int eventY = y1 = e.getY();				
 				mouseMoveTouchMoveAction(e.getSource(), eventX, eventY, ONE_FINGER, 0, 0, 0);
+			} else if (state == TWO_FINGERS && e.getPointerId() == id1 ) {
+				x1 = e.getX();
+				y1 = e.getY();
+				pinch(e);
+				
+			} else if (state == TWO_FINGERS && e.getPointerId() == id2) {
+				x2 = e.getX();
+				y2 = e.getY();
+				pinch(e);
 			}
+		}
+
+		private void pinch(PointerMoveEvent e) {
+			pinchMoveDistance = dist(x1, y1, x2, y2);
+			pinchMoveDirection = direction(x1, y1, x2, y2);
+			mouseMoveTouchMoveAction(e.getSource(), x1, y2, state, pinchStartDistance, pinchMoveDistance, pinchMoveDirection);			
+			pinchStartDistance = pinchMoveDistance;					
 		}
 
 		@Override
@@ -4550,11 +4590,20 @@ public class GraphToolGWT implements EntryPoint, InteractionStub, FacetAware, CB
 			int eventY = e.getY();
 			GWT.log("pointer-down " + eventX + " " + eventY + " " + e.getPointerId());
 			
-			mouseDown = true; // TODO
-			
-			mouseDownTouchStartAction(e.getSource(), eventX, eventY, ONE_FINGER);
-
-			
+			if (state == READY) {
+				state = ONE_FINGER;
+				id1 = e.getPointerId();
+				x1 = eventX;
+				y1 = eventY;
+				mouseDownTouchStartAction(e.getSource(), eventX, eventY, ONE_FINGER);
+			} else if (state == ONE_FINGER ) {
+				state = TWO_FINGERS;
+				id2 = e.getPointerId();
+				x2 = eventX;
+				y2 = eventY;
+				pinchStartDistance = dist(x1, y1, x2, y2);
+				mouseDownTouchStartAction(e.getSource(), x1, y1, TWO_FINGERS);
+			}	
 		}
 		
 	}
