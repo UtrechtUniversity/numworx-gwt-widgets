4ae019f82c33a6b3bf79e661308e026f25b6c917
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2020-06-23 08:56:57 +0000

    shims met chain
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/GeoDefinerGWT/trunk@831 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/geodefinergwt/client/toolbox/ShimModule.java
+++ src/main/java/nl/numworx/geodefinergwt/client/toolbox/ShimModule.java
@@ -1,9 +1,11 @@
 package nl.numworx.geodefinergwt.client.toolbox;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import com.google.gwt.core.client.GWT;
 
+import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 import dagger.multibindings.IntKey;
@@ -20,67 +22,70 @@ import nl.numworx.geodefinergwt.client.ui.PointModel;
 import nl.numworx.geodefinergwt.client.ui.SegmentModel;
 
 @Module
+@SuppressWarnings("unchecked")
 public abstract class ShimModule {
 	
-	@Provides @IntKey(Tools.POINT) @IntoMap @Singleton static
+	@Provides @Singleton @Named("point") static
 	UIShim<? extends Destroyable, Void> point(PointModel ui, Instance instance, Tracker tracker) {
 		return new UIShim<Punt, Void>(ui, instance.getStateConfiguration(), tracker);
 	}
+	@Binds @IntKey(Tools.POINT) @IntoMap abstract UIShim<? extends Destroyable, Void>pointInMap(@Named("point") UIShim<? extends Destroyable, Void> point);
+	
 	@Provides @IntKey(Tools.MIDPOINT) @IntoMap @Singleton static
 	UIShim<? extends Destroyable, Void> midpoint(PointModel ui, Instance instance, Tracker tracker) {
 		return new UIShim<Punt, Void>(ui, instance.getStateConfiguration(), tracker);
 	}
 
 	@Provides @IntKey(Tools.LINE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> line(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> line(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.PARALLEL) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> parallel(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> parallel(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.PERPENDICULAR) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> perpendicular(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> perpendicular(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.BISECTRICE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> bisectrice(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> bisectrice(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.TANGENT) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> tangent(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> tangent(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.POLELINE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> poleline(LineModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> poleline(LineModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 
 	@Provides @IntKey(Tools.SEGMENT) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> segment(SegmentModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> segment(SegmentModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.HALFLINE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> halfline(SegmentModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> halfline(SegmentModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	
 	@Provides @IntKey(Tools.CIRCLE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> circle(CircleModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> circle(CircleModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.CIRCLE_WITH_RADIUS) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> circleWithRadius(CircleModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> circleWithRadius(CircleModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 	@Provides @IntKey(Tools.ARC) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> arc(CircleModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> arc(CircleModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 
 	@Provides @IntKey(Tools.TRIANGLE) @IntoMap @Singleton static
-	UIShim<? extends Destroyable, Void> triangle(CircleModel ui, Instance instance, Tracker tracker) {
-		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker);
+	UIShim<? extends Destroyable, Void> triangle(CircleModel ui, Instance instance, Tracker tracker,@Named("point") UIShim<? extends Destroyable, Void> point) {
+		return new UIShim<Destroyable, Void>(ui, instance.getStateConfiguration(), tracker, (UIShim<Destroyable, Void>) point);
 	}
 
 }
