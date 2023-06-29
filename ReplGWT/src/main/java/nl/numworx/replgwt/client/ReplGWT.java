4ffaa42a15c8459eb21e219f84bdac76f276bb8b
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-06-29 07:56:02 +0200

    Dummy implementation

--- ReplGWT/src/main/java/nl/numworx/replgwt/client/ReplGWT.java
+++ ReplGWT/src/main/java/nl/numworx/replgwt/client/ReplGWT.java
@@ -1,14 +1,111 @@
 package nl.numworx.replgwt.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import nl.uu.fi.dwo.interaction.client.InteractionStub;
+import nl.uu.fi.dwo.interaction.client.OpdrNavIF;
+import nl.uu.fi.dwo.interaction.client.Stub;
+import nl.uu.fi.dwo.interaction.client.event.CBookEvent;
+import nl.uu.fi.dwo.interaction.client.event.CBookEventListener;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class ReplGWT implements EntryPoint {
-  /**
+public class ReplGWT extends SimplePanel implements EntryPoint, InteractionStub, CBookEventListener {
+  private int width;
+  private int height;
+
+/**
    * This is the entry point method.
    */
   public void onModuleLoad() {
+	  RootPanel root = RootPanel.get();
+	  root.add(this);
+	  Stub.publish(this);
   }
+
+@Override
+public HashMap<String, Object> getState() {
+	HashMap<String,Object> state = new HashMap<>();
+	return state;
+}
+
+@Override
+public void setState(HashMap<String, Object> h) {
+}
+
+@Override
+public int getScore() {
+	return 0;
+}
+
+@Override
+public int[][] getScoreObjectives() {
+	return null;
+}
+
+@Override
+public Boolean isCorrect() {
+	return Boolean.TRUE;
+}
+
+@Override
+public void kijkNa() {
+}
+
+@Override
+public void zetNagekeken(boolean b) {
+}
+
+@Override
+public void setCommunicationRoot(OpdrNavIF comRoot) {
+	comRoot.addCBookEventListener("text.program", this);
+}
+
+@Override
+public void zetVolledigeBreedte(int breedte) {
+}
+
+@Override
+public Widget asWidget() {
+	return this;
+}
+
+@Override
+public int getAsHoogte() {
+	return 0;
+}
+
+@Override
+public int getHeight() {
+	return height;
+}
+
+@Override
+public int getWidth() {
+	return width;
+}
+
+@Override
+public void setAsHoogte(int ashoogte) {
+}
+
+@Override
+public void init(int width, int height, Map<String, Object> launchData, Map<String, Number> values) {
+	this.width = width;
+	this.height = height;
+}
+
+@Override
+public void acceptCBookEvent(CBookEvent event) {
+	String program = (String) event.getParameter("content");
+
+	getElement().setInnerText(program);
+}
 }
