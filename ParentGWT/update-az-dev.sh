8dd4c3c5f1cc592749d9a5ca020dc7b0d7560499
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-07-07 09:38:56 +0200

    met AIModelGWT

--- ParentGWT/update-az-dev.sh
+++ ParentGWT/update-az-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
+sh update-app-az-dev.sh AIModelGWT aimodelgwt AIModelGWT
 sh update-app-az-dev.sh AlgebraExprGWT algebraexprgwt AlgebraExprGWT
 sh update-app-az-dev.sh AlgebraPijlenGWT algebrapijlengwt AlgebraPijlenGWT
 sh update-app-az-dev.sh BalansFruitGWT balansfruitgwt BalansFruitGWT
