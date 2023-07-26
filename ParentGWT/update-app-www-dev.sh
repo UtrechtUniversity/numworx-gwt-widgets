c8686acd1f3a39a95524d8d33731c2e9d1f91977
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-07-26 07:44:21 +0200

    update naar test meteen naar azure

--- ParentGWT/update-app-www-dev.sh
+++ ParentGWT/update-app-www-dev.sh
@@ -3,7 +3,6 @@ cd ../$1/target/$3
 PATH=$PATH:/usr/local/bin
 W=/Volumes/fisme-sites/www-dev/dwo/apps
 D=../../../DWOplayer/war
-#G=../../../DWOApp/war/dwo/apps
 S3=s3://test-dwo-nl/apps
 #S3=s3://cds.dwo.nl/apps
 X=echo
@@ -11,13 +10,16 @@ X=
 OPTIONS=-rclD
 if test -f $2/$2.nocache.js
 then
-#$X rsync --delete $OPTIONS $2 $3.css $D/
 $X rsync --delete $OPTIONS $2 $3.css $W/
 #$X rsync --delete $OPTIONS $2 $3.css $W/2014_v1_0/
 #$X rsync --delete $OPTIONS $2 $3.css $W/plantyn/
-#$X rsync --delete $OPTIONS $2 $3.css $G/
+
 $X aws --profile prod s3 cp --recursive --acl public-read $2 $S3/$2
 $X aws --profile prod s3 cp --acl public-read $3.css $S3/$3.css
+
+azcopy sync $3.css https://numworxprod.blob.core.windows.net/cds/apps/$3.css?"$SAS"
+azcopy sync $2/ https://numworxprod.blob.core.windows.net/cds/apps/$2/?"$SAS" --recursive=true --delete-destination true
+
 else
 	echo $2 missing in $(pwd)
 fi
