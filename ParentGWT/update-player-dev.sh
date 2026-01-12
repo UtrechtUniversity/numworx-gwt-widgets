c60a429e8c7d3cacc21ff850a9524610b78b4d14
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2026-01-12 09:21:43 +0100

    add -Pgithub

--- ParentGWT/update-player-dev.sh
+++ ParentGWT/update-player-dev.sh
@@ -5,7 +5,7 @@ PATH=$PATH:/usr/local/bin
 set -e
 cd ../WiskOpdrPlayer;
 
-mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY
+mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY -Pgithub
 (cd target/WiskOpdrPlayer; 
 	
 	azcopy sync DWOplayer.css https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/DWOplayer.css?"$SAS"
@@ -14,13 +14,13 @@ mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.s
 	
 )
 cd ../WidgetPlayer
-mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY
+mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY  -Pgithub
 (cd target/WidgetPlayer;
 
 	azcopy sync WidgetPlayer/ https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/WidgetPlayer/?"$SAS" --recursive=true --delete-destination true
 )
 cd ../PrintPlayer
-mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY
+mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.style=PRETTY  -Pgithub
 (cd target/PrintPlayer;
 
         azcopy sync PrintPlayer/ https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/PrintPlayer/?"$SAS" --recursive=true --delete-destination true
