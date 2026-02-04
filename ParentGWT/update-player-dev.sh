baf114140fd828f8410c59bded46ef415080ebb3
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2026-02-04 15:43:20 +0100

    strange version behaviour
    force version
    also copy PrintPlayer.css

--- ParentGWT/update-player-dev.sh
+++ ParentGWT/update-player-dev.sh
@@ -9,6 +9,7 @@ mvn clean verify -Dgwt.compiler.force=true -Dgwt.compiler.localWorkers=2 -Dgwt.s
 (cd target/WiskOpdrPlayer; 
 	
 	azcopy sync DWOplayer.css https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/DWOplayer.css?"$SAS"
+	azcopy sync PrintPlayer.css https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/PrintPlayer.css?"$SAS"
 	azcopy sync KeyboardGWT.css https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/KeyboardGWT.css?"$SAS"
 	azcopy sync DWOplayer/ https://$ACCOUNT.blob.core.windows.net/$CONTAINER/apps/DWOplayer/?"$SAS" --recursive=true --delete-destination true
 	
